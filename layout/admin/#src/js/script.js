547811f8d9733240983c469fbbd02c8e32d0a264
Andriy <arwel239@gmail.com>
2020-10-03 18:35:31 +0300

    Added pages in admin panel layout: portfolio catalog page, add/edit portfolio page

--- layout/admin/#src/js/script.js
+++ layout/admin/#src/js/script.js
@@ -5,7 +5,5 @@
 $(function () {
     $('.header__hamburger').on("click", function () {
         $('.sidebar').toggleClass("is-active");
-        //$('.navigation__list').toggleClass("is-active");
-        //$('.overlay').toggleClass("is-active");
     });
 });
--- layout/admin/dist/js/script.min.js
+++ layout/admin/dist/js/script.min.js
@@ -1907,7 +1907,5 @@ $(function () {
 $(function () {
     $('.header__hamburger').on("click", function () {
         $('.sidebar').toggleClass("is-active");
-        //$('.navigation__list').toggleClass("is-active");
-        //$('.overlay').toggleClass("is-active");
     });
 });
