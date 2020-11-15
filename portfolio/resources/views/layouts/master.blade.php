2c9e4b9fa109f23bdd241a2a31612d3bc7b31376
Andriy <arwel239@gmail.com>
2020-11-15 14:08:24 +0200

    Fix xcsrf token mismatch

--- portfolio/resources/views/layouts/master.blade.php
+++ portfolio/resources/views/layouts/master.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="@yield('meta-description')">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/assets/main/css/style.min.css"> 
-    <link rel="shortcut icon" href="/assets/favicon.png" type="image/png">
+    <link rel="shortcut icon" href="/images/favicon.png" type="image/png">
 </head>
 <body data-locale="{{ $locale }}">
     <div class="page">
